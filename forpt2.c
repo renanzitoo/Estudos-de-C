a4f7dfcffbf53ff4656a13914644f6d6f867045c
Renan Costa <renan.costa2098@gmail.com>
2021-11-18 16:53:26 -0300

    estudps

--- forpt2.c
+++ forpt2.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main(void){
-int cont, cont2, n;
+    int cont, cont2, n;
 
-printf("Digite o valor de N:");
-scanf("%d",&n);
+    printf("Digite o valor de N:");
+    scanf("%d",&n);
 
-for(cont=1;cont<=n;cont++){
-    for(cont2=1;cont2<=cont;cont2++){
-        printf("X");
-    }
-    printf("\n");
-}
+        for(cont=1;cont<=n;cont++){
+            for(cont2=1;cont2<=cont;cont2++){
+                printf("X");
+            }
+            printf("\n");
+        }
 }
